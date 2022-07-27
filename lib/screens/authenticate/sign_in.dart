c5af8067fe5886fd8b2324e4c02bd25afa9ebae3
Anandk <anand.k4756@gmail.com>
2022-07-27 23:59:55 +0530

    anonymous signin function completed

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -1,3 +1,4 @@
+import 'package:coffeeapp/services/auth.dart';
 import 'package:flutter/material.dart';
 
 class SignIn extends StatefulWidget {
@@ -8,6 +9,8 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
+  final AuthService _auth = AuthService();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,13 +18,20 @@ class _SignInState extends State<SignIn> {
       appBar: AppBar(
         backgroundColor: Colors.brown[400],
         elevation: 0.0,
-        title: Text('Sign in to Specific Coffee'),
+        title: Text('Sign in to Specific Coffee :-)'),
       ),
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
         child: ElevatedButton(
           child: Text('sign in anon'),
-          onPressed: () async {},
+          onPressed: () async {
+            dynamic result = await _auth.signInAnon();
+            if (result == null) {
+              print("error signing in");
+            } else {
+              print("signed in");
+            }
+          },
         ),
       ),
     );
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -8,6 +8,7 @@ class AuthService {
     try {
       UserCredential result = await _auth.signInAnonymously();
       User? user = result.user;
+      return user;
     } catch (e) {
       print(e.toString());
       return null;
